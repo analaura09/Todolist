27154b3cbfc3a0e88eb59b78e17c34df3bca436c
Arthur Holanda <holandaarthur46@gmail.com>
2023-11-18 21:09:55 -0300

    Arquivo de Configuração

---
+++ app/config.py
@@ -0,0 +1,11 @@
+import os
+
+
+config = {
+  "apiKey": os.environ['API_KEY_FIREBASE'],
+  "authDomain": "todolist-72743.firebaseapp.com",
+  "projectId": "todolist-72743",
+  "storageBucket": "todolist-72743.appspot.com",
+  "messagingSenderId": "62911776655",
+  "appId": "1:62911776655:web:bf8175885f0af7e07aedfa"
+};
