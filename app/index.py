ffdc429b73c5883567a2a457da04b67a769c0cf3
Arthur Holanda <holandaarthur46@gmail.com>
2023-11-23 15:05:18 -0300

    Configurando Firebase

--- app/index.py
+++ app/index.py
@@ -1,51 +1,42 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, session, url_for
 import config
-import pyrebase
+import firebase_admin
+from firebase_admin import credentials, auth, firestore
 
 
-firebase = pyrebase.initialize_app(config)
-
-auth = firebase.auth()
-
-
+cred = credentials.Certificate("todolist-72743-firebase-adminsdk-lc4vw-65935aff33.json")
+firebase_admin.initialize_app(cred)
+db_firestore = firestore.client()
 
 app = Flask(__name__)
 
-
-@app.route('acesso_usuario', methods=['GET', 'POST'])
-def acesso_usuario():
-    campo_email = request.form['email']
-    campo_senha = request.form['senha']
-    try:
-        if request.method == 'POST':
-            auth.sign_in_with_email_and_password(campo_email, campo_senha)
-            info_usuario = auth.sign_in_with_email_and_password(campo_email, campo_senha)
-            info_conta = auth.get_account_info(info_usuario['idToken'])
-            if info_conta['user'][0]['emailVerified'] == False:
-                mensagem_verificar = 'Verifique seu email'
-                return render_template('acesso_usuario.html', mensagem_verificar=mensagem_verificar)
-        return render_template('pagina_principal.html')
-    except:
-        mensagem_sem_sucesso = 'Email/Senha Incorretos'
-        return render_template('acesso_usuario.html', mensagem_sem_sucesso=mensagem_sem_sucesso)
-
-@app.route('/cadastro_usuario', methods=['GET', 'POST'])
-def cadastro_usuario():
+@app.route('/login', methods=['GET', 'POST'])
+def login():
     if request.method == 'POST':
-        campo_email = request.form['email']
-        campo_senha = request.form['senha']
-        campo_confirme_sua_senha = request.form['confirme_sua_senha']
+        email = request.form.get('email')
+        password = request.form.get('password')
+        try:
+            user = auth.sign_in_with_email_and_password(email, password)
+            session['user'] = user['idToken']
+            return redirect(url_for('dashboard'))
+        except auth.AuthError as e:
+            return render_template('acesso.html', error=str(e))
 
-        if campo_senha == campo_confirme_sua_senha:
-            try:
-                new_user = auth.create_user_with_email_and_password(campo_email, campo_senha)
-                auth.send_email_verification(new_user['idToken'])
-                return render_template('verifica_email.html')
-            except:
-                conta_existe = 'Esse email já está sendo usado!'
-                render_template('cadastro_usuario.html', mensagem_existe=conta_existe)
+    return render_template('acesso.html')
 
-    return render_template('cadastro_usuario.html')
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        try:
+            user = auth.create_user(email=email, password=password)
+            return redirect(url_for('dashboard'))
+        except auth.AuthError as e:
+            return render_template('register.html', error=str(e))
+
+    return render_template('register.html')
 
 @app.route('/esqueci_senha', methods=['GET', 'POST'])
 def esqueci_senha():
