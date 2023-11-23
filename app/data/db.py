1d7de75f3935a8bd222edcfcde94a338a487e605
Arthur Holanda <holandaarthur46@gmail.com>
2023-11-23 15:05:39 -0300

    Instancia Firebase

--- app/data/db.py
+++ app/data/db.py
@@ -1,21 +1,4 @@
-from flask import Flask, render_template, request
-from google.cloud import firestore
+from firebase_admin import firestore
 
 
-app = Flask(__name__)
-
-# Configurar o Firestore
-db = firestore.Client.from_service_account_json(config)
-
-tarefas_ref = db.collection('tarefas')
-
-def add_tarefa(titulo, descricao):
-    titulo = titulo
-    descricao = descricao
-
-    db.collection('tarefas').add({
-        'titulo': titulo,
-        'descricao': descricao
-    })
-
-add_tarefa('aaa', 'aaa')
+db_firestore = firestore.client()
