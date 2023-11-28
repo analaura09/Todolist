d15060b9b4d30878f7435281495bf94960e7d5f7
Arthur Holanda <holandaarthur46@gmail.com>
2023-11-28 16:45:48 -0300

    Reformulando

--- app/index.py
+++ app/index.py
@@ -1,55 +1,70 @@
-from flask import Flask, redirect, render_template, request, session, url_for
-import config
-import firebase_admin
-from firebase_admin import credentials, auth, firestore
-
-
-cred = credentials.Certificate("todolist-72743-firebase-adminsdk-lc4vw-65935aff33.json")
-firebase_admin.initialize_app(cred)
-db_firestore = firestore.client()
+from flask import Flask, render_template, redirect, request, session, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
 
-@app.route('/login', methods=['GET', 'POST'])
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    senha = db.Column(db.String(120), nullable=False)
+
+
+@app.route('/adasda')
 def login():
+    if 'user_id' in session:
+        return f'Logged in as {User.query.get(session["user_id"]).username}'
+    return 'You are not logged in'
+
+
+@app.route('/', methods=['GET', 'POST'])
+def acesso():
     if request.method == 'POST':
-        email = request.form.get('email')
-        password = request.form.get('password')
-        try:
-            user = auth.sign_in_with_email_and_password(email, password)
-            session['user'] = user['idToken']
-            return redirect(url_for('dashboard'))
-        except auth.AuthError as e:
-            return render_template('acesso.html', error=str(e))
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and check_password_hash(user.password, password):
+            session['user_id'] = user.id
+            flash('Sucesso', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Usuário/Senha Invalidos', 'error')
 
     return render_template('acesso.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        email = request.form.get('email')
-        password = request.form.get('password')
+        username = request.form['username']
+        password = request.form['password']
 
-        try:
-            user = auth.create_user(email=email, password=password)
-            return redirect(url_for('dashboard'))
-        except auth.AuthError as e:
-            return render_template('register.html', error=str(e))
+        if User.query.filter_by(username=username).first():
+            flash('Usuário já existe', 'error')
+        else:
+            hashed_password = generate_password_hash(password, method='sha256')
+            new_user = User(username=username, password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Registrado com sucesso. Efetue o Login', 'success')
+            return redirect(url_for('login'))
 
-    return render_template('register.html')
+    return render_template('cadastro.html')
 
-@app.route('/esqueci_senha', methods=['GET', 'POST'])
-def esqueci_senha():
-    if request.method == 'POST':
-        email = request.form['email']
-        auth.send_password_reset_email(email)
-        return render_template('acesso_usuario.hmtl')
-    return render_template('resetar_senha.html')
-        
 
-@app.route('/')
-def index():
-    return '<h1>Hello, World!</h1>'
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    flash('Logged out successfully', 'success')
+    return redirect(url_for('home'))
+
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
